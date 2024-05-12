5e7825ad27cf53473c285abda080fb51c595dade
sunny bhardwaj <104976449+Sunny12110867@users.noreply.github.com>
2024-05-13 01:30:44 +0530

    Update messageController.js

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -12,21 +12,16 @@ const createMessage = async (req, res) => {
   });
 
   try {
-    // Check if the recipient is online
-    // console.log(receiverId)
-    // console.log(receiver.status)
-    // const receiver = await User.findById(receiverId);
-    // if (receiver.status === "busy") {
-    //   // If the recipient is busy, generate a response using the AI
-    //   const prompt = `give me a response in 5 to 10 word of this message for my chatting application ${text}`;
-    //   const responseText = await getResponse(prompt);
-    //   message.text = responseText;
-    // }
-
-    if(text == 'hello sir'){
-      message.text = "Hello! How can I help you today, sir?";
+    Check if the recipient is online
+    console.log(receiverId)
+    console.log(receiver.status)
+    const receiver = await User.findById(receiverId);
+    if (receiver.status === "busy") {
+      // If the recipient is busy, generate a response using the AI
+      const prompt = `give me a response in 5 to 10 word of this message for my chatting application ${text}`;
+      const responseText = await getResponse(prompt);
+      message.text = responseText;
     }
-   
 
     const response = await message.save();
     res.status(200).json(response);
